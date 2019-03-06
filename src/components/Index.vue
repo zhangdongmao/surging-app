23573910072c42497253504c31c46849972124b6
张东茂 <626943302@qq.com>
2019-03-06 11:46:52 +0800

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <navbar></navbar>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  components:{
+    'Navbar':Navbar,
+    'Sidebar':Sidebar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  position: relative;
+  height: 100%;
+}
+</style>
